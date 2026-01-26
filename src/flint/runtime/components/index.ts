4384810142fd8b75cfe69ba8f38b08e85c2c8ced
UTUBMIR <tytybeteame@gmail.com>
2026-01-26 07:16:58 +0100

    fixed components list.

--- src/flint/runtime/components/index.ts
+++ src/flint/runtime/components/index.ts
@@ -6,3 +6,7 @@
 export { default as Camera } from "./camera";
 export { default as Shape } from "./shape";
 export { default as Transform } from "../transform";
+export { default as Label } from "./label";
+export { BoxCollider } from "./physics/box-collider";
+export { default as Collider } from "./physics/collider";
+export { default as RigidBody } from "./physics/physics-body";
