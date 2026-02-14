3952d8687387eb2e13257a97d5c3fec2887c90eb
UTUBMIR <tytybeteame@gmail.com>
2026-02-14 13:45:52 +0100

    Fixed "missing component Image"

--- src/flint/runtime/components/index.ts
+++ src/flint/runtime/components/index.ts
@@ -7,6 +7,7 @@ export { default as Camera } from "./camera";
 export { default as Shape } from "./shape";
 export { default as Transform } from "../transform";
 export { default as Label } from "./label";
+export { default as Image } from "./image";
 export { default as Collider } from "./physics/collider";
 export { BoxCollider } from "./physics/box-collider";
 export { default as PhysicsBody } from "./physics/physics-body";
