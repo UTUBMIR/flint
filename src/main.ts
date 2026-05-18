affdb65c33ea832d1276dd783129ecbec9ccfebf
UTUBMIR <tytybeteame@gmail.com>
2026-05-18 11:26:36 +0200

    Updated aliases in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import "golden-layout/dist/css/goldenlayout-base.css";
 import "golden-layout/dist/css/themes/goldenlayout-dark-theme.css";
-import { Renderer2D } from "./flint/shared/renderer2d";
-import { System } from "./flint/runtime/system";
-import Editor from "./flint/editor/editor";
-import { BrowserFileSystem } from "./flint/shared/file-system";
+import { Renderer2D } from "@flint/shared/renderer2d";
+import { System } from "@flint/runtime/system";
+import Editor from "@flint/editor/editor";
+import { BrowserFileSystem } from "@flint/shared/file-system";
 import { PhysicsWorld } from "@flint/runtime/physics-world";
-import { initializeEditorLayout } from "./flint/editor/layout";
+import { initializeEditorLayout } from "@flint/editor/layout";
 
 initializeEditorLayout();
 
